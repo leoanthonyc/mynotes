f463ac110d16cd2249ece8afea5df971ea5335b4
Leo Catane <leoanthony.catane@protonmail.com>
2022-04-09 22:07:07 +0800

    Add notes page, add header links

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Notes' });
+  res.render('index', { title: 'Site' });
+});
+
+/* GET home page. */
+router.get('/notes', function(req, res, next) {
+  res.render('notes', { title: 'Notes' });
 });
 
 module.exports = router;
